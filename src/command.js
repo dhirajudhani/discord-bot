540030f01b8c1353dc3e7bc0e130e504fbce30f4
dhirajudhani <dhirajudhani870@gmail.com>
2024-05-19 11:12:59 +0530

    added options and choices func

--- src/command.js
+++ src/command.js
@@ -1,16 +1,40 @@
-import { REST, Routes } from "discord.js";
+import { REST, Routes, ApplicationCommandOptionType } from "discord.js";
 import dotenv from 'dotenv'
 
 dotenv.config()
 const commands = [
   {
-    name: "ping",
-    description: "Replies with Pong!",
-  },
-  {
-    name: "hey",
-    description: "Replies with hey!",
-  },
+    name : 'add',
+    description: 'Add two numbers.',
+    options: [
+      {
+        name:"first-number",
+        description: "The first number.",
+        type : ApplicationCommandOptionType.Number,
+        choices: [
+          {
+            name: 'one',
+            value: 1
+          },
+          {
+            name: 'two',
+            value: 2
+          },
+          {
+            name: 'three',
+            value: 3
+          },
+        ],
+        required : true
+      },
+      {
+        name:"second-number",
+        description: "The second number.",
+        type : ApplicationCommandOptionType.Number,
+        required : true
+      },
+    ]
+  }
 ];
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
--- src/index.js
+++ src/index.js
@@ -28,6 +28,11 @@ client.on('interactionCreate', interaction => {
    if(interaction.commandName === 'hey'){
     interaction.reply("Hey! How you doing");
    }
+   if(interaction.commandName === 'add'){
+    const num1 = interaction.options.get('first-number').value;
+    const num2 = interaction.options.get('second-number').value;
+    interaction.reply(`The sum is ${num1 + num2} `)
+   }
 })
 
 client.login(process.env.TOKEN);
